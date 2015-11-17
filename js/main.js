a3da8963dc6064fe575b282e0c284df0c57822e6
Stewart McCoy <stewart.mccoy@causes.com>
2015-11-17 11:35:17 -0800

    add select class on tabs and add jquery for adding additional questions

--- js/main.js
+++ js/main.js
@@ -1,1 +1,27 @@
+/* ==========================================================================
+   Author's custom scripts
+   ========================================================================== */
 
+
+jQuery(document).ready(function() {
+  $('.tab-option').click(function() {
+    toggleTab(this);
+  });
+
+  function toggleTab(element) {
+    if ($(element).hasClass("selected")) {
+      $(element).removeClass("selected");
+    } else {
+      $(element).siblings().removeClass("selected");
+      $(element).addClass("selected");
+    }
+  };
+
+  $('.add-question').click(function() {
+    addQuestion(this);
+  });
+
+  function addQuestion(element) {
+    $("<input class='form-control specific-question' rows='2' placeholder='e.g. I&#39;m stuck on problem 2 of the assignment'></input>").insertBefore($(element));
+  }
+});
