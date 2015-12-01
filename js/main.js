7233aead153b7caef14e537b9b76335915c5e19d
Stewart McCoy <mccoy.stewart@gmail.com>
2015-11-30 19:01:18 -0800

    remove Chegg Tutors copy in header

--- js/main.js
+++ js/main.js
@@ -24,4 +24,14 @@ jQuery(document).ready(function() {
   function addQuestion(element) {
     $("<input class='form-control specific-question' placeholder='e.g. I&#39;m stuck on problem 2 of the assignment'></input>").insertBefore($(element));
   }
+
+  $(".get-matches").click(function() {
+      var setTitle = $(".subject-search").val();
+      var courseTitle = $(".course-title")[0].innerHTML;
+      if (setTitle.length !== 0) {
+        $(".course-title")[0].innerHTML = setTitle;
+      } else {
+        console.log("cannot be empty!");
+      }
+    })
 });
